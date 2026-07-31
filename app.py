2954529350447ae800f2bd2b8dde8ba419c9376f
Vladislav Boiko <viboyko@edu.hse.ru>
2026-07-31 18:28:23 +0300

    minor changes

--- app.py
+++ app.py
@@ -17,12 +17,9 @@ SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 
 def get_calendar_events(duration: str) -> list[dict] | str:
     creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
     if os.path.exists("token.json"):
         creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
+
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -31,14 +28,13 @@ def get_calendar_events(duration: str) -> list[dict] | str:
                 "credentials.json", SCOPES
             )
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
+
         with open("token.json", "w") as token:
             token.write(creds.to_json())
 
     try:
         service = build("calendar", "v3", credentials=creds)
 
-        # Call the Calendar API
         now = datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(hours=3)))
 
         if duration == "day":
