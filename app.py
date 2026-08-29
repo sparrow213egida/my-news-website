674d394e6293820406fc4deb189f733f2550457a
Vladislav Boiko <viboyko@edu.hse.ru>
2026-08-29 22:07:21 +0300

    updated color logic, token renewal logic

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import datetime
 import os
 
+from google.auth.exceptions import RefreshError
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -12,6 +13,21 @@ from dotenv import load_dotenv
 
 SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 
+load_dotenv()
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+app.calendar_list = os.getenv("CALENDAR_ID_LIST", "primary")
+app.credentials_contents = os.getenv("CREDENTIALS_CONTENTS")
+app.token_contents = os.getenv("TOKEN_CONTENTS")
+
+if app.credentials_contents:
+    with open("credentials.json", "w") as credentials_file:
+        credentials_file.write(app.credentials_contents)
+
+if app.token_contents:
+    with open("token.json", "w") as token_file:
+        token_file.write(app.token_contents)
+
 
 def get_calendar_events(offset: int) -> list[dict] | str:
     creds = None
@@ -20,7 +36,13 @@ def get_calendar_events(offset: int) -> list[dict] | str:
 
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+            try:
+                creds.refresh(Request())
+            except RefreshError:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    "credentials.json", SCOPES
+                )
+                creds = flow.run_local_server(port=0)
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 "credentials.json", SCOPES
@@ -48,8 +70,14 @@ def get_calendar_events(offset: int) -> list[dict] | str:
             calendars = app.calendar_list.split(',')
 
         events = []
+        label_ids = {}
 
         for calendar in calendars:
+            calendar_labels = service.calendars().get(calendarId=calendar).execute()["labelProperties"]["eventLabels"]
+
+            for label in calendar_labels:
+                label_ids[label["id"]] = label["backgroundColor"]
+
             events_result = service.events().list(calendarId=calendar,
                                                    timeMin=start_time.isoformat(),
                                                    timeMax=end_time.isoformat(),
@@ -62,38 +90,15 @@ def get_calendar_events(offset: int) -> list[dict] | str:
 
         request_ans = []
 
-        event_colors = {"0": "#9E9E9E", "1": "#A4BDFC", "2": "#7AE7BF", "3": "#DBADFF", "4": "#FF887C",
-                         "5": "#FBD75B", "6": "#FFB878", "7": "#46D6DB", "8": "#E1E1E1", "9": "#5484ED",
-                         "10": "#51B749", "11": "#DC2127", "12": "#FFADAD", "13": "#FFD6A5", "14": "#FDFFB6",
-                         "15": "#CAFFBF", "16": "#9BF6FF", "17": "#A0C4FF", "18": "#BDB2FF", "19": "#FFC6FF",
-                         "20": "#E74C3C", "21": "#F39C12", "22": "#F1C40F", "23": "#2ECC71", "24": "#1ABC9C",
-                         "25": "#3498DB", "26": "#9B59B6", "27": "#E84393", "28": "#8D6F47", "29": "#95A5A6",
-                         "30": "#4E5D6C", "31": "#865A5A", "32": "#2C3E50"}
-
         for event in events:
             start = event["start"].get("dateTime", event["start"].get("date"))
             end = event["end"].get("dateTime", event["end"].get("date"))
             request_ans.append({"start": start, "end": end, "summary": event["summary"],
-                                "color": event_colors[event.get("colorId", "0")]})
+                                "color": label_ids[event["eventLabelId"]]})
         return request_ans
 
     except HttpError as error:
         return f"An error occurred: {error}"
-
-load_dotenv()
-app = Flask(__name__)
-app.secret_key = os.getenv("SECRET_KEY")
-app.calendar_list = os.getenv("CALENDAR_ID_LIST", "primary")
-app.credentials_contents = os.getenv("CREDENTIALS_CONTENTS")
-app.token_contents = os.getenv("TOKEN_CONTENTS")
-
-if app.credentials_contents:
-    with open("credentials.json", "w") as credentials_file:
-        credentials_file.write(app.credentials_contents)
-
-if app.token_contents:
-    with open("token.json", "w") as token_file:
-        token_file.write(app.token_contents)
 
 
 def shift_time_zone(created_at: str) -> str:
